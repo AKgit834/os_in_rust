cc172c0b1f0eb2214a3b234331b19d5f580036ad
Aaditya Kaushik <aadityakaushik834@gmail.com>
2023-12-24 23:37:50 +0530

    printing message

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,19 @@ fn panic(_panic: &PanicInfo) -> ! {
 loop {}
 }
 
+static HELLO: &[u8] = b"Hello Aaditya";
+
 #[no_mangle] // dont mangle the name of this function.
 //Entry point as the linker looks for function named _start by default. 
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate(){
+        //unsafe tells the compiler that the operations are valid.
+        unsafe{
+            *vga_buffer.offset(i as isize*2)=byte;
+            *vga_buffer.offset(i as isize*2+1)=0xb;
+        }
+    }
 loop {}
 }
