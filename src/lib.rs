fd67d0009295221f3c94b449c2f2e5eaf8222a1c
Aaditya Kaushik <aadityakaushik834@gmail.com>
2023-12-31 23:15:26 +0530

    break point handling

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,10 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-
+#![feature(abi_x86_interrupt)]
 pub mod serial;
 pub mod vga_buffer;
-
+pub mod interrupts;
 use core::panic::PanicInfo;
 
 
@@ -69,12 +69,18 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
+
+
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! 
 {
+    init();
     test_main();
     loop {}
 }
 
-
+pub fn init()
+{
+    interrupts::init_idt();
+}
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,12 @@
 use core::panic::PanicInfo;
 use os::println;
 
+
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello Aaditya{}", "!");
+
 
     #[cfg(test)]
     test_main();
